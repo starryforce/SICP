a5537533f3b3211cac9398eeadfa47367f832602
OF3530 <nichenyu@qianmi.com>
2021-03-16 19:58:29 +0800

    2021/03/16_1

---
+++ 1.2/1.2.2.scm
@@ -0,0 +1,21 @@
+#lang sicp
+
+(define (fib1 n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib1 (- n 1))
+                 (fib1 (- n 2))))))
+
+(fib1 9)
+
+
+(define (fib n)
+  (fib-iter 1 0 n))
+
+(define (fib-iter a b count)
+  (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1))))
+
+
+(fib 9)
